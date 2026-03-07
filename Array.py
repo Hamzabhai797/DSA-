06b69a9a5dbaa0b7985cafd31f6f9329918ad1ae
hamzabhai797 <hamzanoor11111122@gmail.com>
2026-03-07 11:09:04 +0500

    first commit

---
+++ Array.py
@@ -0,0 +1,25 @@
+import array
+# import array as arr
+# from array import *
+# ya 3 type hay array import karne ke liye is ma last wala best hai kyun ke is ma kuch name nahi lana parta aur direct array use kar sakte hain
+
+
+
+# val = array.array('i', [1, 2, 3, 4, 5])
+# print(val)
+
+# val = array.array('i', [1, 2, 3, 4, 5])
+# for i in range(0, 5):
+#     print(val[i])
+
+# val = array.array('i', [1, 2, 3, 4, 5])
+# for i in range(0, 5):
+    # print(val[i], end=' ')
+
+# val = array.array('i', [1, 2, 3, 4, 5])
+# for i in val:
+#    print(i, end=' ')
+
+val = array.array('i', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+for i in range(0, len(val)):
+    print(val[i], end=' ')
