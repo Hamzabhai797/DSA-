bdbf52e5840a2442552c048bb58e635c84bcb139
hamzabhai797 <hamzanoor11111122@gmail.com>
2026-03-13 11:17:53 +0500

    Arrays complete

---
+++ Arrays/dimentionalsArray.py
@@ -0,0 +1,18 @@
+from numpy import *
+
+        # zero dimentional array
+# zero = array(20)
+# print(zero)
+
+
+        # one dimentional array
+# one = array([1, 2, 3, 4, 5])
+# print(one)
+
+        # two dimentional array
+# two = array([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ])
+# print(two)
+
+        # three dimentional array
+three = array([ [[1, 2], [3, 4]], [[5, 6], [7, 8]] ])
+print(three)
---
+++ Arrays/numpyArray.py
@@ -0,0 +1,46 @@
+# import numpy as np
+# import numpy
+from numpy import *
+
+# val = array([1, 2, 3])
+# for i in val:
+#    print(i)
+
+
+# val = array([1, 2, 3], float) # is sa sari values float ma convert ho jainge
+# for i in val:
+#    print(i,  end=' ')
+
+
+# val  = linspace(10, 30, 5) # 10 se 30 tk ke 5 equally spaced values
+# print(val)
+
+# val  = linspace(10, 30, 10)
+
+# for i in val:
+#       print(i, end=' ')
+
+
+         #   arange value in array
+# val = arange(1, 10, 2) # 1 se 10 tk ke odd numbers
+# print(val)
+
+# val = arange(1, 10, 3) 
+# for i in val:
+#      print(i, end=' ')
+
+         #   zeros in array
+# val = zeros(5) # 5 zeros in array
+# for i in val:
+#     print(i, end=' ')
+
+
+         #   ones in array
+# val = ones(5) # 5 ones in array
+# for i in val:
+#     print(i, end=' ')
+
+         #  full in array
+# val = full(5, 7) # 5 se 7 tk ke values
+# for i in val:
+#     print(i, end=' ')
